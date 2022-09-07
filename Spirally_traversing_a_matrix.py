44ebc261733b9f4a86eb6690f647b20453fafe21
Maxwell <maxwellpires2002@gmail.com>
2022-09-07 14:27:32 -0300

    Parametros r e c adicionados

--- Spirally_traversing_a_matrix.py
+++ Spirally_traversing_a_matrix.py
@@ -11,49 +11,58 @@ vetor = []
 def spirallyTraverse(matriz, r, c):
     #Serão 4 interações: Linha para direita, Coluna para baixo, Linha para esquerda, Coluna para cima.
     #A ideia é ir adicionando os elementos ao vetor que deve ser retornado e ir deletando esses elementos da matriz, evitando as repetições.
-    
 
     #Linha para a direita
-    for j in range(len(matriz[0])):
+    for j in range(c):
         vetor.append(matriz[0][j])
 
     matriz.pop(0)
+    r = r-1
 
-    if len(matriz) == 0:
+    if r == 0:
         return vetor
 
-    for k in range(len(matriz)):
-        vetor.append(matriz[k][len(matriz[0])-1])
+    #Coluna para baixo
+    for k in range(r):
+        vetor.append(matriz[k][c-1])
 
     matriz = Transposta(matriz)
-    matriz.pop(len(matriz)-1)
+    matriz.pop(c-1)
     matriz = Transposta(matriz)
-
-    if len(matriz) == 0:
-        return vetor
-
-    for l in range(len(matriz[0])-1, -1, -1):
-        vetor.append(matriz[len(matriz)-1][l])
+    c = c-1
     
-    matriz.pop(len(matriz)-1)
 
-    if len(matriz) == 0:
+    if c == 0:
         return vetor
 
-    for m in range(len(matriz)-1, -1, -1):
+    #Linha para esquerda
+    for l in range(c-1, -1, -1):
+        vetor.append(matriz[r-1][l])
+    
+    matriz.pop(r-1)
+    r = r-1
+    
+
+    if r == 0:
+        return vetor
+
+    #Coluna para cima
+    for m in range(r-1, -1, -1):
         vetor.append(matriz[m][0])
     
     matriz = Transposta(matriz)
     matriz.pop(0)
     matriz = Transposta(matriz)
+    c = c-1
+    
 
-    if len(matriz) == 0:
+    if c == 0:
         return vetor
 
-    return spirallyTraverse(matriz)
+    return spirallyTraverse(matriz, r, c)
 
     
 
-matriz = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+matriz = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 
-print(spirallyTraverse(matriz))
+print(spirallyTraverse(matriz,4,4))
